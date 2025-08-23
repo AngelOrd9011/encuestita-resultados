66a2a64d46002cba7c36c7446db24d338da97edb
Miguel Angel Ordóñez Pérez <m.angel091190@gmail.com>
2025-08-23 15:24:02 -0600

    Se agrega el QR de la encuesta

--- src/components/Answers.jsx
+++ src/components/Answers.jsx
@@ -1,11 +1,27 @@
 import { useEffect, useState } from "react";
+import QRCode from "react-qr-code";
 
 const Answers = ({ respuestas }) => {
   const [answers, setAnswers] = useState();
   useEffect(() => {
     setAnswers(respuestas);
   }, [respuestas]);
-  return <h3>{JSON.stringify(answers)}</h3>;
+  return (
+    <div className="grid">
+      <div className="col-12 md:col-1">
+        {" "}
+        <QRCode
+          value="https://angelord9011.github.io/encuestita"
+          size={256}
+          bgColor="#ffffff"
+          fgColor="#000000"
+        />
+      </div>
+      <div className="col-12 md:col-11">
+        <h3>{JSON.stringify(answers)}</h3>
+      </div>
+    </div>
+  );
 };
 
 export default Answers;
